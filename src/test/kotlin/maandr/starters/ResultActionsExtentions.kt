866a6771754d5e9caa2a9bcc10be6b125ef33286
Mats Andresen <m.andresen-extern@fielmann.com>
2018-06-15 14:00:36 +0200

    Enhance rest api testing facilities.

--- src/test/kotlin/maandr/starters/ResultActionsExtentions.kt
+++ src/test/kotlin/maandr/starters/ResultActionsExtentions.kt
@@ -2,13 +2,26 @@ package maandr.starters
 
 import org.springframework.http.HttpStatus
 import org.springframework.test.web.servlet.ResultActions
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers as matchers
+import org.hamcrest.Matchers as hamcrest
 
+fun ResultActions.print() = this.andDo(MockMvcResultHandlers.print())
 
-fun ResultActions.andPrint() = this.andDo(print())
+fun ResultActions.responseStatus(status: HttpStatus) =
+    this.andExpect(matchers.status().`is`(status.value()))
 
-fun ResultActions.responseStatus(status: HttpStatus) = this.andExpect(status().`is`(status.value()))
+fun ResultActions.jsonPathExists(expression: String) =
+    this.andExpect(matchers.jsonPath(expression, hamcrest.notNullValue()))
+
+fun ResultActions.jsonPathEquals(expression: String, expectedValue: Any) =
+    this.andExpect(matchers.jsonPath(expression, hamcrest.`is`(expectedValue)))
+
+fun ResultActions.headerExists(headerName: String) =
+    this.andExpect(matchers.header().exists(headerName))
+
+fun ResultActions.headerEquals(headerName: String, expectedValue: Any) =
+    this.andExpect(matchers.header().string(headerName, hamcrest.`is`(expectedValue)))
 
 fun ResultActions.andExpectThat(build: ResultActions.() -> Unit): ResultActions {
     this.build()
--- src/test/kotlin/maandr/starters/user/UserRestIntegrationTest.kt
+++ src/test/kotlin/maandr/starters/user/UserRestIntegrationTest.kt
@@ -1,12 +1,14 @@
 package maandr.starters.user
 
 import maandr.starters.*
+import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.*
+import org.junit.After
 import org.junit.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 class UserRestIntegrationTest : AbstractRestIntegrationTest() {
 
@@ -14,6 +16,9 @@ class UserRestIntegrationTest : AbstractRestIntegrationTest() {
     private lateinit var userRepository: UserRepository
 
     private lateinit var existingEntities: Collection<User>
+
+    @After
+    fun cleanup() = userRepository.deleteAllInBatch()
 
     @Test
     fun `should list all existing users`() {
@@ -25,10 +30,52 @@ class UserRestIntegrationTest : AbstractRestIntegrationTest() {
 
         // Then
         resultActions
-            .andPrint()
+            .print()
             .andExpectThat {
                 responseStatus(HttpStatus.OK)
+                jsonPathExists("page")
+                jsonPathEquals("page.totalElements", existingEntities.size)
             }
+    }
+
+    @Test
+    fun `should create a new user`() {
+        // Given
+        val user = anna()
+
+        // When
+        perform(method = HttpMethod.POST, resourcePath = "/users", content = user)
+
+        // Then
+        resultActions
+            .print()
+            .andExpectThat {
+                responseStatus(HttpStatus.CREATED)
+                headerExists(HttpHeaders.LOCATION)
+                jsonPathEquals("name", user.name)
+                jsonPathEquals("age", user.age)
+            }
+    }
+
+    @Test
+    fun `should delete existing user`() {
+        // Given
+        givenExistingUsers()
+        val user = existingEntities.first()
+
+        // When
+        perform(method = HttpMethod.DELETE, resourcePath = "/users/${user.id}")
+
+        // Then
+        resultActions
+            .print()
+            .andExpectThat {
+                responseStatus(HttpStatus.NO_CONTENT)
+            }
+        assertThat(userRepository.count()).isEqualTo(existingEntities.size - 1L)
+        assertThat(userRepository.findAll()).containsExactlyInAnyOrderElementsOf(existingEntities
+                        .filter { it.id != user.id  }
+                        .toList())
     }
 
     fun givenExistingUsers() {
