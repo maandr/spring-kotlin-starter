b06a54092fff2168f632797c46259a0a0b58d677
Mats Andresen <m.andresen-extern@fielmann.com>
2018-06-20 11:50:01 +0200

    Remove unused annotation.

--- src/main/kotlin/maandr/starters/user/User.kt
+++ src/main/kotlin/maandr/starters/user/User.kt
@@ -1,6 +1,5 @@
 package maandr.starters.user
 
-import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
@@ -10,7 +9,6 @@ import javax.persistence.Id
 data class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @JsonIgnore
     val id: Long = 0,
 
     val name: String = "",
